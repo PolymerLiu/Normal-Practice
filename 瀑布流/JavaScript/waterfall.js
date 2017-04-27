fb8f859693707cd1f3d8f67de22f53f74c7d633c
Polymer Liu <17771770679@163.com>
2017-04-27 23:59:52 +0800

    mmark

--- 瀑布流/JavaScript/waterfall.js
+++ 瀑布流/JavaScript/waterfall.js
@@ -2,10 +2,12 @@ window.onload=function(){
 	waterfall('main','box');
 	var dataInt={"data":[{"src":'0.jpg'},{"src":'1.jpg'},{"src":'2.jpg'},{"src":'3.jpg'}]};
 
-	window.onscroll = function(){
+	window.onscroll = function(){   //检测页面是否滚动
 		if (checkScrollSlide()) {
 			var oParent=document.getElementById('main');
-			for (var i = 0; i < dataInt.data.length; i++) {  //把获取到的图片添加到现有图片的后边
+			for (var i = 0; i < dataInt.data.length; i++) {//dataInt是对象，并无length属性，dataInt.data属性才有length属性
+
+			//把获取到的图片添加到现有图片的后边
 				var oBox=document.createElement('div');
 				oBox.className='box';
 				oParent.appendChild(oBox);
@@ -16,26 +18,24 @@ window.onload=function(){
 
 				var oImg=document.createElement('img');
 				oImg.src="./images/"+dataInt.data[i].src;
-				oPic.appendChild(oImg);
-				
+				oPic.appendChild(oImg);				
 			}
-			waterfall('main','box');
+			waterfall('main','box');  ///再次调用waterfall函数使新加载的图片以瀑布流的方式排布
 		}
 	}
 }
 
 function waterfall(parent,box){
 	var oParent=document.getElementById(parent);
-	var oBoxs=getByClass(oParent,box);//将main下所有class为box的元素取出来
+	var oBoxs=getByClass(oParent,'box');//将main下所有class为box的元素取出来
 	var oBoxW=oBoxs[0].offsetWidth;
 	var cols=Math.floor(document.documentElement.clientWidth/oBoxW);// 计算整个页面显示的列数（页面宽/box的宽）
 	oParent.style.cssText='width:'+oBoxW*cols+'px; margin:0 auto';// 设置main 的宽度
 
-
 	var hArr=[];//存放每一列高度的数组
 	for (var i = 0; i < oBoxs.length; i++) {
 		if (i<cols) {
-			hArr.push(oBoxs[i].offsetHeight);
+			hArr.push(oBoxs[i].offsetHeight);  //获取的是第一行图片的高度
 		}else{
 			var minH=Math.min.apply(null,hArr);
 			var index=getMinhIndex(hArr,minH);
@@ -43,10 +43,9 @@ function waterfall(parent,box){
 			oBoxs[i].style.position='absolute';  
 			oBoxs[i].style.top=minH+'px';
 			oBoxs[i].style.left=oBoxs[index].offsetLeft+'px';
-			hArr[index]+=oBoxs[i].offsetHeight;
+			hArr[index]+=oBoxs[i].offsetHeight;  //获取的是每一列图片的高度
 		}
 	}
-
 }
 
 function getByClass(parent,clsName){
@@ -59,6 +58,8 @@ function getByClass(parent,clsName){
 	}
 	return boxArr;
 }
+
+//获取最矮的图片的索引
 function getMinhIndex(arr,val){
 	for(var i in arr){
 			if (arr[i]==val) {
@@ -66,14 +67,13 @@ function getMinhIndex(arr,val){
 		}
 	}
 }
-
-
+  
 // 检测是否具备滚动加载数据的条件
 function checkScrollSlide(){
 	var oParent=document.getElementById('main');
 	var oBoxs=getByClass(oParent,'box');
 	var lastBoxH=oBoxs[oBoxs.length-1].offsetTop+Math.floor(oBoxs[oBoxs.length-1].offsetHeight/2);//最后一个图片离页面顶端的高度
 	var scrollTop=document.body.scrollTop || document.documentElement.scrollTop;  //页面顶端离窗口顶部的高度，即滚动条滚动的距离
-	var height=document.body.clientHeight || document.documentElement.clientHeight;  //窗口高度
+	var height=document.body.clientHeight || document.documentElement.clientHeight;  //窗口高度   //获取混杂模式 || 标准模式 高度
 	return(lastBoxH<scrollTop+height)?true:false;
 }
