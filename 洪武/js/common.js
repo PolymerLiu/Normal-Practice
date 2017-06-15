517c9c1d564c2d2673ddbc9353ace0f735b155df
JeffreyLiu <17771770679@163.com>
2017-06-15 14:47:15 +0800

    last

--- 洪武/js/common.js
+++ 洪武/js/common.js
@@ -6,6 +6,7 @@ var back_top=document.getElementById("back_top");
 back_top.style.display="none";
 var windowHeight =window.innerWidth || document.documentElement.clientWidth;
 
+var nav=document.getElementsByTagName("nav")[0];
 //var ul=document.getElementById("ul");
 // var lis=ul.children;
 
@@ -19,10 +20,25 @@ window.onscroll= function () {
     }else{
         back_top.style.display="none";
     }
+
+    //  固定导航栏
+    if (scrollTop>nav.offsetTop)
+    {
+        nav.style.position='fixed';
+        nav.style.top=0;
+    }else
+    {
+        nav.style.position='absolute';
+        nav.style.top="50px";
+    }
+
 };
-back_top.addEventListener("click", function () {
+/*back_top.addEventListener("click", function () {
     document.offsetTop=0;
-});
+});*/
+back_top.onclick= function () {
+    document.offsetTop=0;
+};
 
 //  导航栏高亮当前页
 
