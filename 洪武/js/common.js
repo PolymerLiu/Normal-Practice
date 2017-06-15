c1c38f2826170413f2248f89633d1a5bc1e2bd4d
JeffreyLiu <17771770679@163.com>
2017-06-15 15:19:07 +0800

    终版demo

--- 洪武/js/common.js
+++ 洪武/js/common.js
@@ -4,16 +4,22 @@
  */
 var back_top=document.getElementById("back_top");
 back_top.style.display="none";
-var windowHeight =window.innerWidth || document.documentElement.clientWidth;
-
+var windowHeight =window.innerHeight || document.documentElement.clientHeight;
+var index_banner=document.getElementById("index_banner");
+//console.log(windowHeight);
 var nav=document.getElementsByTagName("nav")[0];
+console.log(windowHeight);
 //var ul=document.getElementById("ul");
 // var lis=ul.children;
-
+if (windowHeight<=638)
+{
+    index_banner.style.height="100vh";
+}
 
 //  返回按钮
 window.onscroll= function () {
     var scrollTop=window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    console.log(scrollTop);
     if (scrollTop>windowHeight/2)
     {
         back_top.style.display="block";
